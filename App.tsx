cf057d1eb78f7a821a0de9e86093788f0eaaf57c
Antonio Iaccarino <antonio.iaccarino@inloop.com.au>
2024-06-13 10:29:39 +1000

    feat: react native CLI verion

--- App.tsx
+++ App.tsx
@@ -1,57 +1,50 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
+  FlatList,
+  Pressable,
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
   Text,
+  TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {useAsyncStorage} from '@react-native-async-storage/async-storage';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+import {MMKV, useMMKVString} from 'react-native-mmkv';
+export const storage = new MMKV();
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+import {Realm, RealmProvider, useRealm, useQuery} from '@realm/react';
+
+class Task extends Realm.Object {
+  _id!: Realm.BSON.ObjectId;
+  value!: string;
+
+  static generate(value: string) {
+    return {
+      _id: new Realm.BSON.ObjectId(),
+      value,
+      createdAt: new Date(),
+    };
+  }
+
+  static schema = {
+    name: 'Task',
+    primaryKey: '_id',
+    properties: {
+      _id: 'objectId',
+      value: 'string',
+    },
+  };
+}
+function AppWrapper() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <RealmProvider schema={[Task]}>
+      <App />
+    </RealmProvider>
   );
 }
 
@@ -62,6 +55,31 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  // AsyncStorage
+  const [asyncStoreValue, setAsyncStoreValue] = useState<string | null>(null);
+  const {getItem, setItem} = useAsyncStorage('@storage_key');
+
+  const readItemFromStorage = async () => {
+    const item = await getItem();
+    setAsyncStoreValue(item);
+  };
+
+  const writeItemToStorage = async (newValue: string): Promise<void> => {
+    await setItem(newValue);
+    setAsyncStoreValue(newValue);
+  };
+
+  useEffect(() => {
+    readItemFromStorage();
+  });
+
+  // MMKV
+  const [mmkvValue, setMMKVValue] = useMMKVString('@MMKV');
+
+  // Realm
+  const realm = useRealm();
+  const tasks = useQuery(Task);
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
@@ -71,48 +89,74 @@ function App(): React.JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <View style={{margin: 30}}>
+            <Text style={{fontSize: 20}}>Async Storage</Text>
+            <Text>Async Storage, Current value: {asyncStoreValue}</Text>
+            <TouchableOpacity
+              style={{padding: 10, backgroundColor: '#37c7cc'}}
+              onPress={() =>
+                writeItemToStorage(Math.random().toString(36).substr(2, 5))
+              }>
+              <Text>Update Async Storage value</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={{margin: 30}}>
+            <Text style={{fontSize: 20}}>MMKV</Text>
+            <Text>MMKV, Current value: {mmkvValue}</Text>
+            <TouchableOpacity
+              style={{padding: 10, backgroundColor: '#37c7cc'}}
+              onPress={() =>
+                setMMKVValue(Math.random().toString(36).substr(2, 5))
+              }>
+              <Text>Update MMKV value</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={{margin: 30}}>
+            <Text style={{fontSize: 20}}>Realm</Text>
+            <FlatList
+              data={tasks}
+              keyExtractor={item => item._id.toHexString()}
+              renderItem={({item}) => {
+                return (
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      margin: 10,
+                    }}>
+                    <Text style={{paddingHorizontal: 10}}>{item.value}</Text>
+                    <Pressable
+                      onPress={() => {
+                        realm.write(() => {
+                          realm.delete(item);
+                        });
+                      }}>
+                      <Text>{'🗑️'}</Text>
+                    </Pressable>
+                  </View>
+                );
+              }}
+            />
+            <TouchableOpacity
+              style={{padding: 10, backgroundColor: '#37c7cc'}}
+              onPress={() => {
+                realm.write(() => {
+                  realm.create(
+                    'Task',
+                    Task.generate(Math.random().toString(36).substr(2, 5)),
+                  );
+                });
+              }}>
+              <Text>Add Realm value</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+export default AppWrapper;
